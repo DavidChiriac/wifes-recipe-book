ea6b2d34ced3716c6a281b514426999886aeec15
CrcDavid <118164428+CrcDavid@users.noreply.github.com>
2025-06-25 14:39:52 +0300

    Create hompage

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DeviceService } from '../shared/services/device.service';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [CommonModule, ButtonModule, RouterModule],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
+  isMobile!: boolean;
 
+  constructor(private readonly deviceService: DeviceService) {
+    this.isMobile = deviceService.isMobile();
+  }
 }
