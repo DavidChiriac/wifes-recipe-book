d56d3f6768079eea78096b845bf3e4454a088c22
CrcDavid <118164428+CrcDavid@users.noreply.github.com>
2025-07-03 15:04:32 +0300

    Auto-generated commit

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{e,f as t,h as o,i as p,k as m}from"./chunk-ZI55IXMB.mjs";import{fc as r}from"./chunk-TO6QGAZZ.mjs";import"./chunk-O6D5ERBR.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
