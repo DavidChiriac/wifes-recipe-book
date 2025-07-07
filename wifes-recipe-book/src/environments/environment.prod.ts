1cd3b205d725bfe1d5dbef3c49ba96405f2156f4
CrcDavid <118164428+CrcDavid@users.noreply.github.com>
2025-07-07 15:35:35 +0300

    correct api url

--- wifes-recipe-book/src/environments/environment.prod.ts
+++ wifes-recipe-book/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   prod: true,
-  apiUrl: 'https://strapi-wifes-recipe-book.onrender.com/',
+  apiUrl: 'https://strapi-wifes-recipe-book.onrender.com',
   clientID:
     '109546617287-8v3o6quekpeituq54h5cebmg3ushfvk7.apps.googleusercontent.com',
 };
