081b3a74e1c91870f0ab25d30c9b196c60f77fb3
CrcDavid <118164428+CrcDavid@users.noreply.github.com>
2025-07-08 15:24:16 +0300

    remove recommended recipes

--- wifes-recipe-book/src/app/home-page/home-page.component.ts
+++ wifes-recipe-book/src/app/home-page/home-page.component.ts
@@ -1,21 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DeviceService } from '../shared/services/device.service';
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
-import { IRecipe } from '../shared/interfaces/recipe.interface';
-import { RecipeCardComponent } from '../shared/components/recipe-card/recipe-card.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [CommonModule, ButtonModule, RouterModule, RecipeCardComponent],
+  imports: [CommonModule, ButtonModule, RouterModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
   isMobile!: boolean;
-
-  recommendedRecipes: IRecipe[] = [];
 
   constructor(
     private readonly deviceService: DeviceService,
