758e4e42a5dc83790ef9d7022eeac904a9e46b78
CrcDavid <118164428+CrcDavid@users.noreply.github.com>
2025-07-08 15:17:22 +0300

    remove recommended

--- wifes-recipe-book/src/app/home-page/home-page.component.ts
+++ wifes-recipe-book/src/app/home-page/home-page.component.ts
@@ -4,35 +4,22 @@ import { DeviceService } from '../shared/services/device.service';
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
 import { IRecipe } from '../shared/interfaces/recipe.interface';
-import { RecipesService } from '../shared/services/recipes.service';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { RecipeCardComponent } from '../shared/components/recipe-card/recipe-card.component';
 
-@UntilDestroy()
 @Component({
   selector: 'app-home-page',
   imports: [CommonModule, ButtonModule, RouterModule, RecipeCardComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
   isMobile!: boolean;
 
   recommendedRecipes: IRecipe[] = [];
 
   constructor(
     private readonly deviceService: DeviceService,
-    private readonly recipesService: RecipesService
   ) {
     this.isMobile = deviceService.isMobile();
-  }
-
-  ngOnInit(): void {
-    this.recipesService
-      .getRecommendedRecipes()
-      .pipe(untilDestroyed(this))
-      .subscribe((recipes) => {
-        this.recommendedRecipes = [...recipes];
-      });
   }
 }
