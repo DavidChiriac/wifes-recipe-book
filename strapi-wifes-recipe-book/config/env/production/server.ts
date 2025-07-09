c8bcccde26d86dc8b3b07c7cde85671a44f9867c
CrcDavid <118164428+CrcDavid@users.noreply.github.com>
2025-07-09 10:24:47 +0300

    Add cron job to keep the server alive

--- strapi-wifes-recipe-book/config/env/production/server.ts
+++ strapi-wifes-recipe-book/config/env/production/server.ts
@@ -1,4 +1,10 @@
+import cronTasks from "../../cron";
+
 export default ({ env }) => ({
 	host: env("HOST", "0.0.0.0"),
 	url: env("", "https://uplifting-happiness-124a800a1d.strapiapp.com"),
+	cron: {
+		enabled: true,
+		tasks: cronTasks,
+	},
 });
