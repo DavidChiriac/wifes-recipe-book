6897eb3ea178a5fd5b2592a0604277f733e94859
CrcDavid <118164428+CrcDavid@users.noreply.github.com>
2025-07-10 15:31:57 +0300

    Delete cron job

--- strapi-wifes-recipe-book/config/env/production/server.ts
+++ strapi-wifes-recipe-book/config/env/production/server.ts
@@ -1,10 +1,4 @@
-import cronTasks from "config/cron";
-
 export default ({ env }) => ({
 	host: env("HOST", "0.0.0.0"),
 	url: env("", "https://uplifting-happiness-124a800a1d.strapiapp.com"),
-	cron: {
-		enabled: true,
-		tasks: cronTasks,
-	},
 });
